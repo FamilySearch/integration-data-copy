0c27a3129eb0fb9932bec2e965dac611b75d2928
Justin <justincyork@gmail.com>
2015-09-09 16:04:05 +0000

    load starting person

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ var productionClient = getFSClient('sandbox'),
 
 $(function(){
   
+  initializeAuthentication();
+  setupStart();
+  showStart();
+  
+});
+
+/**
+ * Setup auth controls and events; detect the initial auth state.
+ */
+function initializeAuthentication(){
   var $prodAuth = $('#prod-auth').click(function(){
     productionClient.getAccessToken().then(function(token){
       $prodAuth.find('.no-auth').hide();
       $prodAuth.find('.auth').show();
       Cookies.set('production-token', token);
+      showStart();
     });
   });
   
@@ -21,6 +32,7 @@ $(function(){
       $sandboxAuth.find('.no-auth').hide();
       $sandboxAuth.find('.auth').show();
       Cookies.set('sandbox-token', token);
+      showStart();
     });
   });
   
@@ -28,9 +40,55 @@ $(function(){
     $sandboxAuth.find('.no-auth').hide();
     $sandboxAuth.find('.auth').show();
   }
-  
-});
+}
 
+/**
+ * Bind events to the start id input
+ */
+function setupStart(){
+  $('#start-search-btn').click(function(){
+    var personId = $('#startPersonId').val().trim();
+    if(!/^[A-Z0-9]+-[A-Z0-9]+$/.test(personId)){
+      $('#start').addClass('has-error');
+    } else {
+      getStartPersonSummary(personId);
+      $('#start').removeClass('has-error');
+    }
+  });
+}
+
+/**
+ * Get and display the start person's name and lifespan
+ */
+function getStartPersonSummary(personId){
+  $('#start-search-btn').prop('disabled', true);
+  $('#start-person-details .name').text('');
+  $('#start-person-details .lifespan').text('');
+  $('#start-person-details .error').html('');
+  
+  productionClient.getPerson(personId).then(function(response){
+    $('#start-search-btn').prop('disabled', false);
+    var person = response.getPerson();
+    $('#start-person-details .name').text(person.getDisplayName());
+    $('#start-person-details .lifespan').text(person.getDisplayLifeSpan());
+  }).catch(function(){
+    $('#start-search-btn').prop('disabled', false);
+    $('#start-person-details .error').html('<div class="alert alert-danger">Unable to load person ' + personId + '.</div>');
+  });
+}
+
+/**
+ * Show the start person section if we are authenticated in both environments.
+ */
+function showStart(){
+  if(productionClient.hasAccessToken() && sandboxClient.hasAccessToken()){
+    $('#start').show();
+  }
+}
+
+/**
+ * Create an FS SDK client for the given environment.
+ */
 function getFSClient(environment){
   var config = {
       client_id: 'a02j00000098ve6AAA',
